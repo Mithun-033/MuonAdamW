5d13b7b6aeb238e8721612873e79204d3641035a
Mithun Kannaa <mithunkannaa609@gmail.com>
2026-06-22 10:37:24 +0530

    shld do role-based splitting, add all the optim.Optimizer functionalities

--- MuonAdamW/optimizer.py
+++ MuonAdamW/optimizer.py
@@ -1,30 +1,69 @@
+'''
+This module implements the MuonAdamW optimizer, which combines the AdamW and Muon optimization algorithms.
+The MuonAdamW optimizer allows for flexible configuration of both AdamW and Muon components,
+enabling users to tailor the optimization process to their specific needs. The optimizer supports three modes of operation:
+1. Shape-wise: Parameters are categorized based on their shape, with multi-dimensional parameters optimized using
+   Muon and one-dimensional parameters optimized using AdamW.
+2. Role-wise: Parameters are categorized based on their role in the model. Embeddings, Norms are optimized using AdamW, 
+    while all other 2d parameters are optimized using Muon.
+3. Custom: Users can explicitly specify which parameters should be optimized with Muon and which with AdamW.
+The optimizer also allows for different learning rates for the Muon and AdamW components, with options
+to maintain the original learning rate or to match the RMS of AdamW updates.
+
+'''
+
 from torch import nn
 import torch.optim as optim
 from typing import Literal,Iterator
+from arguments import AdamW,Muon
+from helper_utils import make_adamw, make_muon
 
 class MuonAdamW(optim.Optimizer):
+    '''Optimizer that combines AdamW and Muon optimizers.'''
     def __init__(self,
                 model : Iterator[nn.Parameter],
                 lr : float = 1e-3,
                 mode : Literal["shape-wise","role-wise","custom"]="role-wise",
-                adamw_betas : tuple[float] = (0.9,0.99),
-                adamw_eps : float = 1e-8,
-                adamw_weight_decay : float = 0.1,
-                adamw_amsgrad : bool = False,
-                adamw_maximize : bool = False, 
-                adamw_foreach : bool = False,
-                adamw_capturable : bool = False,
-                adamw_differentiable : bool =False,
-                adamw_fused : bool = False,
-                muon_weight_decay : float = 1e-2,
-                muon_momentum : float = 0.95,
-                muon_nesterov : bool = True,
-                muon_ns_coefficients : tuple[float] = (3.4445, -4.775, 2.0315),
-                muon_eps : float = 1e-7,
-                muon_ns_steps : int =5,
-                muon_lr_multiplier : str | int = "original",
+                muon_parameters : list[nn.Parameter] | None = None,
+                adam_parameters : list[nn.Parameter] | None = None,
+                adam_args : AdamW | None = None,
+                muon_args : Muon | None = None,
+                muon_lr_multiplier : Literal["original","match_rms_adamw"] | float = "original",
                 ):
+    
         super(MuonAdamW, self).__init__()
+        
+        if adam_args is None:
+            adam_args = AdamW()
+        if muon_args is None:
+            muon_args = Muon()
+
+        self.mode = mode
+        self.muon_lr_multiplier = muon_lr_multiplier  
+
+        if self.mode == "custom":
+            assert muon_parameters is not None and adam_parameters is not None, '''For custom mode, both muon_parameters and adam_parameters must be provided.'''
+            self.muon_params = muon_parameters
+            self.adam_params = adam_parameters
+
+        elif self.mode == "shape-wise":
+            self.muon_params = []
+            self.adam_params = []
+            for param in model:
+                if param.ndim >= 2:
+                    self.muon_params.append(param)
+                else:
+                    self.adam_params.append(param)  
+
+        elif self.mode == "role-wise":
+            ...
+
+        self.adamw_optimizer = make_adamw(lr, self.adam_params, adam_args)
+        self.muon_optimizer = make_muon(lr, self.muon_params, muon_lr_multiplier, muon_args)
+        
+        
+        
+
 
         
 
