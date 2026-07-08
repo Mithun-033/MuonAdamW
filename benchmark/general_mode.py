58f9aab52e73c69c7fa318d6ecfaa5eeb21c0ef4
Mithun Kannaa <mithunkannaa609@gmail.com>
2026-07-08 18:59:19 +0530

    class name change

--- benchmark/general_mode.py
+++ benchmark/general_mode.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from MuonAdamW.optimizer import MuonAdamW
-from MuonAdamW.arguments import AdamW
+from MuonAdamW.arguments import AdamwArgs
 from torch.optim import AdamW as TorchAdamW
 
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -94,7 +94,7 @@ muonadamw=MuonAdamW(
     model1,
     mode="general",
     lr=learning_rate,
-    adam_args=AdamW(weight_decay=0.01)
+    adam_args=AdamwArgs(weight_decay=0.01)
 )
 
 adam=TorchAdamW(model2.parameters(),lr=learning_rate,weight_decay=0.01)
